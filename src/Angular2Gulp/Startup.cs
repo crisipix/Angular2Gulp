525979c222a684f318eef68e55fb0c9ec0933099
crisipix <wongc5@gmail.com>
2016-05-24 09:47:38 -0400

    Updated - use file server shouldn't be in the use
    Updated - use file server shouldn't be in the use  app.Use(async (context, next) =>

--- src/Angular2Gulp/Startup.cs
+++ src/Angular2Gulp/Startup.cs
@@ -31,8 +31,7 @@ namespace Angular2Gulp
         }
 
         /*
-            https://blogs.msdn.microsoft.com/webdev/2015/03/19/customize-external-web-tools-in-visual-studio-2015/
-            //If you’re using Angular 2 with ASP.NET static file serving, you’ll have no doubt run into the fact that once you’ve navigated to 
+              //If you’re using Angular 2 with ASP.NET static file serving, you’ll have no doubt run into the fact that once you’ve navigated to 
             //a different Angular route, you can’t refresh the page. You’ll get a 404 error because the new URL no longer points to a valid file.
             //Instead we want to route all requests for Angular routes to the root page, usually index.html.Angular will then go figure out which page to load based on the URL.
             //http://benjii.me/2016/01/angular2-routing-with-asp-net-core-1/
@@ -45,26 +44,27 @@ namespace Angular2Gulp
                 if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
                 {
                     //context.Request.Path = "/index.html";
-                    context.Request.Path = "/wwwroot/index.html";
+                    context.Request.Path = "/index.html";
                     await next();
                 }
-
-                // Serve wwwroot as root
-                app.UseFileServer();
-
-                // Serve /node_modules as a separate root (for packages that use other npm modules client side)
-                //app.UseFileServer(new FileServerOptions()
-                //{
-                //    // Set root of file server
-                //    // FileProvider = new PhysicalFileProvider(Path.Combine(environment.ApplicationBasePath, "node_modules")),
-                //    FileProvider = new PhysicalFileProvider(Path.Combine(environment.ApplicationBasePath, "lib")),
-                //    // Only react to requests that match this path
-                //    //RequestPath = "/node_modules",
-                //    RequestPath = "/lib",
-                //    // Don't expose file system
-                //    EnableDirectoryBrowsing = false
-                //});
             });
+
+
+            // Serve wwwroot as root
+            app.UseFileServer();
+
+            //Serve / node_modules as a separate root (for packages that use other npm modules client side)
+            //app.UseFileServer(new FileServerOptions()
+            //{
+            //    // Set root of file server
+            //    // FileProvider = new PhysicalFileProvider(Path.Combine(environment.ApplicationBasePath, "node_modules")),
+            //    FileProvider = new PhysicalFileProvider(Path.Combine(environment.ApplicationBasePath, "/wwwroot/libs")),
+            //    // Only react to requests that match this path
+            //    //RequestPath = "/node_modules",
+            //    RequestPath = "/wwwroot/libs",
+            //    // Don't expose file system
+            //    EnableDirectoryBrowsing = false
+            //});
 
             //http://stackoverflow.com/questions/30250764/how-to-force-all-requests-in-asp-net-5-mvc-project-to-redirect-to-root
             // this will force all paths to root. IE http://localhost:65399/detail/3 turns to http://localhost:65399/
